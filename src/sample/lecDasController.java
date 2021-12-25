9ada6b3bb5da7ad9d1ede094ece8d59cd0a0a656
Michelle-Lohwt <michellelohwt0799@gmail.com>
2021-12-25 20:55:55 +0800

    add lecturer dashboard page

---
+++ src/sample/lecDasController.java
@@ -0,0 +1,40 @@
+package sample;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class lecDasController extends Controller implements Initializable {
+
+  @FXML
+  private Text name, nric, position, school, staffID, status;
+
+  public void LogOut(MouseEvent event) throws IOException {
+    switchTo(event, "logout.fxml");
+  }
+
+  public void openBrowser(MouseEvent event) throws URISyntaxException, IOException {
+    openLink(event);
+  }
+
+  private void lecInfo() {
+    name.setText("ABC");
+    nric.setText("12345");
+    staffID.setText("L12345");
+    status.setText("ACTIVE");
+    school.setText("School of Computer Science");
+    position.setText("Senior Lecturer");
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    lecInfo();
+  }
+
+}
