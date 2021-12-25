5e530d303c785b49f77e9092247471d8ced7b7b0
Michelle-Lohwt <michellelohwt0799@gmail.com>
2021-12-25 10:38:23 +0800

    remove unnecessary library

--- src/sample/welcomeController.java
+++ src/sample/welcomeController.java
@@ -7,7 +7,6 @@ import java.net.URISyntaxException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.input.MouseEvent;
 
