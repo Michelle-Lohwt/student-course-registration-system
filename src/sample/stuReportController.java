f7135c20706176cce59d8457fa1fba275245bc78
Michelle-Lohwt <michellelohwt0799@gmail.com>
2021-12-26 12:22:11 +0800

    add student contact us page

---
+++ src/sample/stuReportController.java
@@ -0,0 +1,24 @@
+package sample;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javafx.scene.input.MouseEvent;
+
+public class stuReportController extends Controller{
+  public void StuDashboard(MouseEvent event) throws IOException {
+    switchTo(event, "stuDashboard.fxml");
+  }
+
+  public void StuCourseRegistration(MouseEvent event) throws IOException{
+    switchTo(event, "courseRegistration.fxml");
+  }
+
+  public void LogOut(MouseEvent event) throws IOException {
+    switchTo(event, "logout.fxml");
+  }
+
+  public void openBrowser(MouseEvent event) throws URISyntaxException, IOException {
+    openLink(event);
+  }
+}
