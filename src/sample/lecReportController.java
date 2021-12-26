7f8af2dcba6907ecbcd119e13f91911cf7da70e7
Michelle-Lohwt <michellelohwt0799@gmail.com>
2021-12-26 12:22:38 +0800

    add lecturer contact us page

---
+++ src/sample/lecReportController.java
@@ -0,0 +1,25 @@
+package sample;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javafx.scene.input.MouseEvent;
+
+public class lecReportController extends Controller {
+  public void LecDashboard(MouseEvent event) throws IOException {
+    switchTo(event, "lecDashboard.fxml");
+  }
+
+  public void StuList(MouseEvent event) throws IOException {
+    switchTo(event, "stuList.fxml");
+  }
+
+  public void LogOut(MouseEvent event) throws IOException {
+    switchTo(event, "logout.fxml");
+  }
+
+  public void openBrowser(MouseEvent event) throws URISyntaxException, IOException {
+    openLink(event);
+  }
+
+}
