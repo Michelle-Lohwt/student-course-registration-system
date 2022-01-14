adddbc7c66361e0a65f9ae50283cd0a6382bf79e
Zi Qiang <qiang2344@gmail.com>
2022-01-15 01:58:39 +0800

    commented input file function, on hold for Derek's push, need his code to try input file

--- src/sample/classes/Student.java
+++ src/sample/classes/Student.java
@@ -1,5 +1,7 @@
 package sample.classes;
+//import java.io.File;
 import java.io.FileNotFoundException;
+//import java.util.Scanner;
 
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
@@ -11,6 +13,20 @@ public class Student {
     public static void main(String[] args) throws FileNotFoundException{
         List list1= new List();
         
+
+        //input the text file for the course list
+        // try {
+        //     File fileObj = new File("student1.txt");
+        //     Scanner fileReader = new Scanner(fileObj);
+        //     while (fileReader.hasNextLine()) {
+        //       registeredCourse.getItems().list1.add(fileReader.nextLine());
+        //     }
+        //     fileReader.close();
+        //   } catch (FileNotFoundException e) {
+        //     System.out.println("An error occurred.");
+        //     e.printStackTrace();
+        //   }
+
         //Insert course list at here
         list1.add("Java");
         list1.add("Android");
