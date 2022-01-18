4fbafd741f1ca772ef9d2c59a502f25eba4f6e03
Michelle-Lohwt <michellelohwt0799@gmail.com>
2022-01-18 11:32:32 +0800

    fix: indentation and comment

--- src/sample/classes/StudentController.java
+++ src/sample/classes/StudentController.java
@@ -18,73 +18,67 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
-public class StudentController implements Initializable{
-@FXML
-private Label label;
-@FXML
-private AnchorPane anchorid;
-@FXML 
-private TextField textfield;
+public class StudentController implements Initializable {
+  @FXML
+  private Label label;
+  @FXML
+  private AnchorPane anchorid;
+  @FXML
+  private TextField textfield;
 
-@FXML
-public void handleButtonAction(ActionEvent event) throws IOException{
+  @FXML
+  public void handleButtonAction(ActionEvent event) throws IOException {
     final DirectoryChooser dirchooser = new DirectoryChooser();
 
     Stage stage = (Stage) anchorid.getScene().getWindow();
 
     File file = dirchooser.showDialog(stage);
 
-        //input the text file for the course list
-        // try {
-        //     File fileObj = new File("student1.txt");
-        //     Scanner fileReader = new Scanner(fileObj);
-        //     while (fileReader.hasNextLine()) {
-        //       registeredCourse.getItems().list1.add(fileReader.nextLine());
-        //     }
-        //     fileReader.close();
-        //   } catch (FileNotFoundException e) {
-        //     System.out.println("An error occurred.");
-        //     e.printStackTrace();
-        //   }
-    if(file !=null)
-    {   
-        List list1= new List();
-        list1.add("Java");
-        list1.add("Android");
-        list1.add("Kotlin");
-        list1.add("Android");
-        
-        System.out.println("Path: " + file.getAbsolutePath());
-        textfield.setText(file.getAbsolutePath());
-        
-        
-        PdfWriter pdfWriter = new PdfWriter(file.getAbsolutePath() + "/CourseList.pdf");
+    // input the text file for the course list
+    // try {
+    // File fileObj = new File("student1.txt");
+    // Scanner fileReader = new Scanner(fileObj);
+    // while (fileReader.hasNextLine()) {
+    // registeredCourse.getItems().list1.add(fileReader.nextLine());
+    // }
+    // fileReader.close();
+    // } catch (FileNotFoundException e) {
+    // System.out.println("An error occurred.");
+    // e.printStackTrace();
+    // }
+    if (file != null) {
+      List list1 = new List();
+      list1.add("Java");
+      list1.add("Android");
+      list1.add("Kotlin");
+      list1.add("Android");
 
-        PdfDocument pdfDocument = new PdfDocument(pdfWriter);
-        pdfDocument.addNewPage();
+      System.out.println("Path: " + file.getAbsolutePath());
+      textfield.setText(file.getAbsolutePath());
 
-        Document document = new Document(pdfDocument);
-        document.add(list1);
+      PdfWriter pdfWriter = new PdfWriter(file.getAbsolutePath() + "/CourseList.pdf");
 
- 
-        document.close();
-      
+      PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+      pdfDocument.addNewPage();
+
+      Document document = new Document(pdfDocument);
+      document.add(list1);
+
+      document.close();
+
     }
 
+  }
 
-  
-}
-
-@FXML
-public void confirmbutton (ActionEvent event){
+  @FXML
+  public void confirmbutton(ActionEvent event) {
     Stage stage = (Stage) anchorid.getScene().getWindow();
     stage.close();
 
-}
+  }
 
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
 
-@Override
-public void initialize(URL url, ResourceBundle rb){
-    // TODO
-}
+  }
 }
