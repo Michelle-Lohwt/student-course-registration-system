5ff3b28970a63eba35819ff3494a692cff9921fe
Michelle-Lohwt <michellelohwt0799@gmail.com>
2022-01-20 17:02:03 +0800

    feat: dashboard redesign

--- src/sample/stuDashController.java
+++ src/sample/stuDashController.java
@@ -5,15 +5,28 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Text;
 
 public class stuDashController extends Controller implements Initializable {
 
   @FXML
-  private Text acd_status, campus, cgpa, major, matric, minor, name, nric, programme, school, sem_reg, year;
+  private ImageView profilePic;
+
+  @FXML
+  private TextField name, nric, matric, cgpa;
+
+  @FXML
+  private ChoiceBox<String> acd_status, sem_reg, year, school, campus, programme, major, minor;
+
+  @FXML
+  private JFXButton editInfoButton, saveButton;
 
   public void LogOut(MouseEvent event) throws IOException {
     switchTo(event, "logout.fxml");
@@ -31,23 +44,71 @@ public class stuDashController extends Controller implements Initializable {
     openLink();
   }
 
-  private void stuInfo() {
-    name.setText("Michelle");
-    nric.setText("12345");
-    matric.setText("149104");
-    acd_status.setText("ACTIVE");
-    sem_reg.setText("201");
-    cgpa.setText("3.77");
-    year.setText("Year 3");
-    school.setText("School of Computer Science");
-    campus.setText("Main Campus");
-    programme.setText("Computer Science");
-    major.setText("Software Engineering");
-    minor.setText("Accounting");
+  private void defaultInfo() {
+    // Please change the matric number
+    matric.setText("12345");
+    acd_status.setValue("Active");
+  }
+
+  public void editStuInfo() {
+    name.setEditable(true);
+    nric.setEditable(true);
+    cgpa.setEditable(true);
+
+    name.setStyle("-fx-border-color: #eb7231");
+    nric.setStyle("-fx-border-color: #eb7231");
+    cgpa.setStyle("-fx-border-color: #eb7231");
+
+    name.setDisable(false);
+    nric.setDisable(false);
+    cgpa.setDisable(false);
+    acd_status.setDisable(false);
+    sem_reg.setDisable(false);
+    year.setDisable(false);
+    school.setDisable(false);
+    campus.setDisable(false);
+    programme.setDisable(false);
+    major.setDisable(false);
+    minor.setDisable(false);
+  }
+
+  public void saveInfo() {
+    name.setEditable(false);
+    nric.setEditable(false);
+    cgpa.setEditable(false);
+
+    name.setStyle("-fx-border-color: default");
+    nric.setStyle("-fx-border-color: default");
+    cgpa.setStyle("-fx-border-color: default");
+
+    name.setDisable(true);
+    nric.setDisable(true);
+    cgpa.setDisable(true);
+    acd_status.setDisable(true);
+    sem_reg.setDisable(true);
+    year.setDisable(true);
+    school.setDisable(true);
+    campus.setDisable(true);
+    programme.setDisable(true);
+    major.setDisable(true);
+    minor.setDisable(true);
+  }
+
+  private void ChoiceBoxItem() {
+    acd_status.getItems().addAll("Active", "Probationary");
+    sem_reg.getItems().addAll("17/1", "17/2", "18/1", "18/2", "19/1", "19/2", "20/1", "20/2", "21/1", "21/2", "22/1",
+        "22/2");
+    year.getItems().addAll("Year 1", "Year 2", "Year 3", "Year 4", "Others");
+    school.getItems().addAll("School of Computer Science", "School of Mathematical Sciences", "School of Management");
+    campus.getItems().addAll("Main Campus", "Health Campus", "Engineering Campus");
+    programme.getItems().addAll("BSc Computer Science", "BSc Mathematics", "BSc Management");
+    major.getItems().addAll("Software Engineering", "Intelligent Computing", "Computing Infrastructure");
+    minor.getItems().addAll("Accounting", "Mathematics", "Electives");
   }
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    stuInfo();
+    ChoiceBoxItem();
+    defaultInfo();
   }
 }
