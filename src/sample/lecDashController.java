46fc0e4c5ac5a13fbe7f8ddfd5a1f8f79ea5478c
Michelle-Lohwt <michellelohwt0799@gmail.com>
2022-01-25 22:49:05 +0800

    feat: modify lecDashController according to new interface

--- src/sample/lecDashController.java
+++ src/sample/lecDashController.java
@@ -5,15 +5,28 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Text;
 
 public class lecDashController extends Controller implements Initializable {
 
   @FXML
-  private Text name, nric, position, school, staffID, status;
+  private TextField name, nric, staffID;
+
+  @FXML
+  private JFXButton editInfoButton, saveButton;
+
+  @FXML
+  private ChoiceBox<String> emp_status, position, school, campus;
+
+  @FXML
+  private ImageView profilePic;
 
   public void LogOut(MouseEvent event) throws IOException {
     switchTo(event, "logout.fxml");
@@ -31,18 +44,53 @@ public class lecDashController extends Controller implements Initializable {
     openLink();
   }
 
-  private void lecInfo() {
-    name.setText("ABC");
-    nric.setText("12345");
+  private void defaultInfo() {
     staffID.setText("L12345");
-    status.setText("ACTIVE");
-    school.setText("School of Computer Science");
-    position.setText("Senior Lecturer");
+    emp_status.setValue("Active");
+  }
+
+  public void editInfo() {
+    name.setEditable(true);
+    nric.setEditable(true);
+
+    name.setStyle("-fx-border-color: #eb7231");
+    nric.setStyle("-fx-border-color: #eb7231");
+
+    name.setDisable(false);
+    nric.setDisable(false);
+    emp_status.setDisable(false);
+    position.setDisable(false);
+    school.setDisable(false);
+    campus.setDisable(false);
+  }
+
+  public void saveInfo() {
+    name.setEditable(false);
+    nric.setEditable(false);
+
+    name.setStyle("-fx-border-color: default");
+    nric.setStyle("-fx-border-color: default");
+
+    name.setDisable(true);
+    nric.setDisable(true);
+    emp_status.setDisable(true);
+    position.setDisable(true);
+    school.setDisable(true);
+    campus.setDisable(true);
+  }
+
+  private void ChoiceBoxItem() {
+    emp_status.getItems().addAll("Active", "Research", "Further Study");
+    position.getItems().addAll("Lecturer", "Senior Lecturer", "Professor", "Associate Professor",
+        "Honorary Associate Professor", "Honorary Professor");
+    school.getItems().addAll("School of Computer Science", "School of Mathematical Sciences", "School of Management");
+    campus.getItems().addAll("Main Campus", "Health Campus", "Engineering Campus");
   }
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    lecInfo();
+    ChoiceBoxItem();
+    defaultInfo();
   }
 
 }
